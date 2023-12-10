c5948e7292658d3dcbbc4ce0e106adb6db12035e
Richard Roy <richard.alex.roy@gmail.com>
2023-12-10 11:57:53 -0500

    basic melee inheritance for player

--- CIS343/Python/Project/melee.py
+++ CIS343/Python/Project/melee.py
@@ -1,7 +1,34 @@
-from . import entity
-import pygame as pg
 
-class Melee(entity.Entity):
-    def __init__(self):
-        pass
+import pygame as pg
+from entity import Entity as Entity
+
+class Melee(Entity):
+    def __init__(self, startLocation, team):
+        super().__init__(startLocation, team)
+        self.__idleFrameSet = []
+        self.__walkFrameSet = []
+        self.__actionFrameSet = []
+        self.__deathFrameSet = []
+        self.frameNumber = 0
+        self.width = 32
+        self.height = 32
+        
+        frameRange = 9
+        row = 0
+        self.addIdleFrameSet('./assets/warrior spritesheet calciumtrice.png', frameRange, row, self.width, self.height)
+        row = 1
+        self.addIdleFrameSet('./assets/warrior spritesheet calciumtrice.png', frameRange, row, self.width, self.height)
+        row = 2
+        self.addWalkFrameSet('./assets/warrior spritesheet calciumtrice.png', frameRange, row, self.width, self.height)
+        row = 3
+        self.addActionFrameSet('./assets/warrior spritesheet calciumtrice.png', frameRange, row, self.width, self.height)
+        row = 4    
+        self.addDeathFrameSet('./assets/warrior spritesheet calciumtrice.png', frameRange, row, self.width, self.height)
+        
+        
+        
+        # default sprite and image creation
+        startFrame = self.idleFrameSet[self.frameNumber]
+        self.updateFrame(startFrame)
+        
     
